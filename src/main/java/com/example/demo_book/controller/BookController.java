196f143c7d0eafaf5564e9ea25f8a07093ec2bac
Hsuyuhung <kariya94@gmail.com>
2023-01-03 11:00:56 +0800

    ニューブランチテスト

--- src/main/java/com/example/demo_book/controller/BookController.java
+++ src/main/java/com/example/demo_book/controller/BookController.java
@@ -20,17 +20,14 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 
-	// 創建書籍資料
 	@PostMapping(value = "api/new_release")
 	public BookRes newRelease(@RequestBody BookReq req) {
-		
-		// 如任意項目為空，則回傳錯誤訊息
+
 		if (!StringUtils.hasText(req.getISBN()) || !StringUtils.hasText(req.getName())
 				|| !StringUtils.hasText(req.getAuthor()) || !StringUtils.hasText(req.getCategory())) {
 			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
 
-		// 判斷ISBN長度
 		switch (req.getISBN().length()) {
 
 		case 10:
@@ -41,7 +38,6 @@ public class BookController {
 			return new BookRes(BookMessageCode.ISBN_LENGTH.getMessage());
 		}
 
-		// 價格與初始庫存不得小於等於0
 		if (req.getPrice() <= 0 || req.getPurchase() <= 0) {
 			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
@@ -49,30 +45,26 @@ public class BookController {
 		Book book = bookService.newRelease(req.getISBN(), req.getName(), req.getAuthor(), req.getCategory(),
 				req.getPrice(), req.getPurchase());
 
-		// 如ISBN已存在，則回傳錯誤訊息
 		if (book == null) {
 			return new BookRes(BookMessageCode.ISBN_EXISTED.getMessage());
 		}
 
-		return new BookRes(book, BookMessageCode.CREATE_INFO_SUCCESSFUL.getMessage());
+		return new BookRes(book, BookMessageCode.SUCCESSFUL.getMessage());
 	}
 
-	// 更新書籍資料
 	@PostMapping(value = "api/update_book_info")
 	public BookRes updateBookInfo(@RequestBody BookReq req) {
 
 		if (!StringUtils.hasText(req.getISBN())) {
 			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
-		
-		// 如所有項目為空(無變更)，則回傳錯誤訊息
+
 		if (!StringUtils.hasText(req.getName()) && !StringUtils.hasText(req.getName())
 				&& !StringUtils.hasText(req.getAuthor()) && !StringUtils.hasText(req.getCategory())
 				&& req.getPrice() == 0 && req.getPurchase() == 0 && req.getSales() == 0) {
-			return new BookRes(BookMessageCode.ITEM_WRONG.getMessage());
+			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
 
-		// 如價格、庫存、銷售小於0，則回傳錯誤訊息
 		if (req.getPrice() < 0 || req.getPurchase() < 0 || req.getSales() < 0) {
 			return new BookRes(BookMessageCode.NUMBER_ERRO.getMessage());
 		}
@@ -81,38 +73,33 @@ public class BookController {
 				req.getPrice(), req.getPurchase(), req.getSales());
 	}
 
-	// 透過類別搜尋書籍資料
 	@PostMapping(value = "api/search_category")
 	public BookRes searchCategory(@RequestBody BookReq req) {
 
-		// 類別無輸入值，則回傳錯誤訊息
 		if (!StringUtils.hasText(req.getCategory())) {
 			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
 
-		// 如此類別無書籍資料，則回傳錯誤訊息
 		List<Book> bookList = bookService.searchCategory(req.getCategory());
 		if (bookList == null) {
 			return new BookRes(BookMessageCode.NO_RESULT.getMessage());
 		}
 
-		return new BookRes(bookList, BookMessageCode.SEARCH_SUCCESSFUL.getMessage());
+		return new BookRes(bookList, BookMessageCode.SUCCESSFUL.getMessage());
 	}
 
-	// 透過ISBN、書名、作者名等任意項目搜尋書籍資料(根據密碼分為書籍商、使用者2種顯示版本)
 	@PostMapping(value = "api/find_by_ISBN_Name_Author")
 	public BookRes findByISBNOrNameOrAuthor(@RequestBody BookReq req) {
 
-		// 如所有項目為空，則回傳錯誤訊息
 		if (!StringUtils.hasText(req.getISBN()) && !StringUtils.hasText(req.getName())
 				&& !StringUtils.hasText(req.getAuthor())) {
 			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
 
-		// 如查無任何資料，則回傳錯誤樳
 		BookRes listBook = bookService.getBookInfoByISBNOrNameOrAuthor(req.getPassword(), req.getISBN(), req.getName(),
 				req.getAuthor());
 		if (listBook == null) {
+			// 脤磐彆
 			return new BookRes(BookMessageCode.NO_RESULT.getMessage());
 		}
 
@@ -120,57 +107,47 @@ public class BookController {
 				req.getAuthor());
 	}
 
-	// 更新庫存
 	@PostMapping(value = "api/update_purchase")
 	public BookRes updatePurchase(@RequestBody BookReq req) {
-		
-		// 如ISBN輸入為空，則回傳錯誤訊息
+
 		if (!StringUtils.hasText(req.getISBN())) {
 			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
 
-		// 如庫存量小於0，則回傳錯誤訊息
 		if (req.getPurchase() < 0) {
-			return new BookRes(BookMessageCode.NUMBER_ERRO.getMessage());
+			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
 
-		// 如查無此ISBN，則回傳錯誤訊息
 		Book book = bookService.updatePurchase(req.getISBN(), req.getPurchase());
 		if (book == null) {
 			return new BookRes(BookMessageCode.ISBN_NULL.getMessage());
 		}
 
-		return new BookRes(book, BookMessageCode.UPDATE_SUCCESSFUL.getMessage());
+		return new BookRes(book, BookMessageCode.SUCCESSFUL.getMessage());
 	}
 
-	// 更新價格
 	@PostMapping(value = "api/update_price")
 	public BookRes updatePrice(@RequestBody BookReq req) {
 
-		// 如價格輸入為空，則回傳錯誤訊息
 		if (!StringUtils.hasText(req.getISBN())) {
 			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
 
-		// 如價格小於0，則回傳錯誤訊息
 		if (req.getPrice() < 0) {
-			return new BookRes(BookMessageCode.NUMBER_ERRO.getMessage());
+			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
 
-		// 如查無此ISBN，則回傳錯誤訊息
 		Book book = bookService.updatePrice(req.getISBN(), req.getPrice());
 		if (book == null) {
 			return new BookRes(BookMessageCode.ISBN_NULL.getMessage());
 		}
 
-		return new BookRes(book, BookMessageCode.UPDATE_SUCCESSFUL.getMessage());
+		return new BookRes(book, BookMessageCode.SUCCESSFUL.getMessage());
 	}
 
-	// 銷售書籍
 	@PostMapping(value = "/api/sales")
 	public BookRes sales(@RequestBody BookReq req) {
 
-		// 如無購書資訊(Map形式：ISBN+購買數量)，則回傳錯誤訊息
 		if (req.getBuyBook().isEmpty()) {
 			return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 		}
@@ -178,7 +155,6 @@ public class BookController {
 		return bookService.sales(req.getBuyBook());
 	}
 
-	// 銷售排行前5
 	@PostMapping(value = "api/best_seller")
 	public List<Book> bestSeller() {
 
--- src/main/java/com/example/demo_book/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/demo_book/service/impl/BookServiceImpl.java
@@ -24,11 +24,9 @@ public class BookServiceImpl implements BookService {
 	@Autowired
 	private BookDao bookDao;
 
-	// 創建書籍資料
 	@Override
 	public Book newRelease(String ISBN, String name, String author, String category, Integer price, Integer purchase) {
 
-		// 如果為已存在ISBN，則回傳null，於controller進行判斷，回傳錯誤訊息
 		if (bookDao.existsById(ISBN)) {
 			return null;
 		}
@@ -37,7 +35,6 @@ public class BookServiceImpl implements BookService {
 		return bookDao.save(book);
 	}
 
-	// 更新書籍資料
 	@Override
 	public BookRes updateBookInfo(String ISBN, String name, String author, String category, Integer price,
 			Integer purchase, Integer sales) {
@@ -47,24 +44,20 @@ public class BookServiceImpl implements BookService {
 		Optional<Book> bookOp = bookDao.findById(ISBN);
 		Book book = bookOp.get();
 
-		// 如查無ISBN，則回傳錯誤訊息
 		if (!bookOp.isPresent()) {
 			return new BookRes(BookMessageCode.ISBN_WRONG.getMessage());
 		}
 
-		// 透過私有方法進行防呆
 		setParams(book, name, author, category, price, purchase, sales);
 		bookDao.save(book);
 		res.setBook(book);
-		res.setMessage(BookMessageCode.UPDATE_SUCCESSFUL.getMessage());
+		res.setMessage(BookMessageCode.SUCCESSFUL.getMessage());
 		return res;
 	}
 
-	// 判斷輸入值是否存回資料庫、是否小於0之私有方法
 	private void setParams(Book book, String name, String author, String category, Integer price, Integer purchase,
 			Integer sales) {// parameter
 
-		// 如有輸入值，則將輸入值存回資料庫
 		if (StringUtils.hasText(name)) {
 			book.setName(name);
 		}
@@ -77,7 +70,6 @@ public class BookServiceImpl implements BookService {
 			book.setCategory(category);
 		}
 
-		// 如價格、庫存、銷售大於等於0，則將輸入值存回資料庫
 		if (price >= 0) {
 			book.setPrice(price);
 		}
@@ -91,28 +83,21 @@ public class BookServiceImpl implements BookService {
 		}
 	}
 
-	// 透過類別搜尋書籍資料
 	@Override
 	public List<Book> searchCategory(String category) {
 
-		// 將輸入值以「,」切分(split)，並去除字串頭尾空白(trim)，最後將輸入值放入Set去除重複
 		String[] stringList = category.split(",");
 		Set<String> bookSet = new HashSet<>();
 		for (String item : stringList) {
 			bookSet.add(item.trim());
 		}
 
-		// 將上方取得之Set進行遍歷，比對輸入值跟資料庫是否有相符值
 		List<Book> newBookList = new ArrayList<>();
 		for (String item1 : bookSet) {
 			List<Book> bookList = bookDao.findByCategoryContaining(item1);
-			
-			// 如果輸入值不存在於資料庫，則回傳null，於controller進行判斷，回傳錯誤訊息
 			if (bookList.isEmpty()) {
 				return null;
 			}
-			
-			// 為不顯示Category與Sales，故使用遍歷將List內所有此2項目設定成null，並透過JsonInclude使其不顯示
 			for (Book item2 : bookList) {
 				item2.setCategory(null);
 				item2.setSales(null);
@@ -123,41 +108,32 @@ public class BookServiceImpl implements BookService {
 		return newBookList;
 	}
 
-	// 透過ISBN、書名、作者名等任意項目搜尋書籍資料(根據密碼分為書籍商、使用者2種顯示版本)
 	@Override
 	public BookRes getBookInfoByISBNOrNameOrAuthor(String password, String ISBN, String name, String author) {
 
 		BookRes res = new BookRes();
 		List<Book> bookList = bookDao.findByISBNOrNameOrAuthor(ISBN, name, author);
-		// 書籍商版本之List
 		List<Book> consumerBookList = new ArrayList<>();
-		// 使用者版本之List
 		List<Book> storeBookList = new ArrayList<>();
 
-		// 如無輸入密碼，則回傳錯誤訊息
 		if (!StringUtils.hasText(password)) {
 			res.setMessage(BookMessageCode.ITEM_EMPTY.getMessage());
 			return res;
 		}
 
-		// 如查無任何資料，則回傳null，於controller進行判斷，回傳錯誤訊息
 		if (bookList.isEmpty()) {
 			return null;
 		}
 
-		// 如果輸入正確密碼(0)，則回傳書籍商版本資料
 		if (password.equals("0")) {
-			
-			// 為不顯示Category項目，故進行遍歷設定為null不顯示
 			for (Book item : bookList) {
 				item.setCategory(null);
 				storeBookList.add(item);
 			}
 			res.setStoreBookList(storeBookList);
-			res.setMessage(BookMessageCode.SEARCH_SUCCESSFUL.getMessage());
+			res.setMessage(BookMessageCode.SUCCESSFUL.getMessage());
 			return res;
 		} else {
-			// 如未輸入正確密碼，則回傳使用者版本資料
 			for (Book item : bookList) {
 				item.setCategory(null);
 				item.setPurchase(null);
@@ -166,18 +142,15 @@ public class BookServiceImpl implements BookService {
 			}
 
 			res.setConsumerBookList(consumerBookList);
-			res.setMessage(BookMessageCode.SEARCH_SUCCESSFUL.getMessage());
+			res.setMessage(BookMessageCode.SUCCESSFUL.getMessage());
 			return res;
 		}
 	}
 
-	// 更新庫存
 	@Override
 	public Book updatePurchase(String ISBN, Integer purchase) {
 
 		Optional<Book> bookOp = bookDao.findById(ISBN);
-		
-		// 如ISBN存在，則將取得之新庫存量存回資料庫，存完後再將不顯示之項目設定為null
 		if (bookOp.isPresent()) {
 			Book book = bookOp.get();
 			book.setPurchase(purchase);
@@ -187,17 +160,13 @@ public class BookServiceImpl implements BookService {
 			return book;
 		}
 
-		// 如果ISBN不存在，則回傳null，於controller進行判斷，回傳錯誤訊息
 		return null;
 	}
 
-	// 更新價格
 	@Override
 	public Book updatePrice(String ISBN, Integer price) {
 
 		Optional<Book> bookOp = bookDao.findById(ISBN);
-		
-		// 如ISBN存在，則將取得之新價格存回資料庫，存完後再將不顯示之項目設定為null
 		if (bookOp.isPresent()) {
 			Book book = bookOp.get();
 			book.setPrice(price);
@@ -207,51 +176,40 @@ public class BookServiceImpl implements BookService {
 			return book;
 		}
 
-		// 如果ISBN不存在，則回傳null，於controller進行判斷，回傳錯誤訊息
 		return null;
 	}
 
-	// 銷售書籍
 	@Override
 	public BookRes sales(Map<String, Integer> buyBook) {
 
 		BookRes res = new BookRes();
 		List<ConsumerReceiptInfo> receipt = new ArrayList<>();
-		// 預設總金額為0
 		int total = 0;
 
-		// 遍歷Map取得ISBN與購買數量，並計算金額
 		for (Map.Entry<String, Integer> item : buyBook.entrySet()) {
-			
-			// 如Key值(ISBN)為空，則回傳錯誤訊息
 			if (!StringUtils.hasText(item.getKey())) {
 				return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 			}
 
 			Optional<Book> bookOp = bookDao.findById(item.getKey());
-			// 如Key值(ISBN)不存在，則回傳錯誤訊息
 			if (!bookOp.isPresent()) {
 				return new BookRes(BookMessageCode.ISBN_NULL.getMessage());
 			}
 
 			Integer value = item.getValue();
-			// 如Value值(購買數量)小於0，則回傳錯誤訊息
 			if (value == null || value < 0) {
 				return new BookRes(BookMessageCode.ITEM_EMPTY.getMessage());
 			}
 
-			// 如Value值(購買數量)大於Purchase(庫存量)，則回傳錯誤訊息
 			if (value > bookOp.get().getPurchase()) {
 				return new BookRes(BookMessageCode.NO_STOCK.getMessage());
 			}
 
-			// 根據購買數量，更新Sales(增加銷售)與Purchase(減少庫存)數量
 			Book storeBook = bookOp.get();
 			storeBook.setSales(storeBook.getSales() + value);
 			storeBook.setPurchase(storeBook.getPurchase() - value);
 			bookDao.save(storeBook);
 
-			// 計算購買總金額
 			total += bookOp.get().getPrice() * value;
 
 			ConsumerReceiptInfo consumerBuyBook = new ConsumerReceiptInfo(storeBook.getName(), storeBook.getISBN(),
@@ -262,19 +220,16 @@ public class BookServiceImpl implements BookService {
 
 		res.setConsumerReceipt(receipt);
 		res.setTotalPrice(total);
-		res.setMessage(BookMessageCode.SALE_SUCCESSFUL.getMessage());
+		res.setMessage(BookMessageCode.SUCCESSFUL.getMessage());
 		return res;
 
 	}
 
-	// 銷售排行前5
 	@Override
 	public List<Book> bestSeller() {
 
-		// 使用JPA DAO方法排序銷售前5五之書籍資訊
 		List<Book> bookList = bookDao.findTop5ByOrderBySalesDesc();
 		List<Book> newBookList = new ArrayList<>();
-		// 將不顯示之項目設定為null
 		for (Book item : bookList) {
 			item.setCategory(null);
 			item.setPurchase(null);
